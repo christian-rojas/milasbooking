0e854c549f604fe61405a881227dab0bb54399fd
christian-rojas <christian.ici17@gmail.com>
2024-03-26 11:12:44 -0300

    add another calendar

--- components/Services.vue
+++ components/Services.vue
@@ -4,36 +4,54 @@
     <p class="text-muted d-flex justify-content-start">
       **Se solicitará completar una ficha con los antecedentes de su mascotas
     </p>
-    <ul class="nav nav-pills p-2" id="pills-tab" role="tablist">
-      <li class="nav-item" role="presentation">
-        <button
-          class="nav-link active"
-          id="pills-home-tab"
-          data-bs-toggle="pill"
-          data-bs-target="#pills-home"
-          type="button"
-          role="tab"
-          aria-controls="pills-home"
-          aria-selected="true"
-        >
-          Consulta General
-        </button>
-      </li>
-      <li class="nav-item" role="presentation">
-        <button
-          class="nav-link"
-          id="pills-profile-tab"
-          data-bs-toggle="pill"
-          data-bs-target="#pills-profile"
-          type="button"
-          role="tab"
-          aria-controls="pills-profile"
-          aria-selected="false"
-        >
-          Gastroenterología
-        </button>
-      </li>
-    </ul>
+    <div class="row">
+      <div class="col-md-12">
+        <ul class="nav nav-pills flex-column p-2 d-block m-auto" id="pills-tab" role="tablist">
+          <li class="nav-item d-grid" role="presentation">
+            <button
+              class="nav-link active"
+              id="pills-home-tab"
+              data-bs-toggle="pill"
+              data-bs-target="#pills-home"
+              type="button"
+              role="tab"
+              aria-controls="pills-home"
+              aria-selected="true"
+            >
+              Consulta General
+            </button>
+          </li>
+          <li class="nav-item d-grid" role="presentation">
+            <button
+              class="nav-link"
+              id="pills-profile-tab"
+              data-bs-toggle="pill"
+              data-bs-target="#pills-profile"
+              type="button"
+              role="tab"
+              aria-controls="pills-profile"
+              aria-selected="false"
+            >
+              Gastroenterología
+            </button>
+          </li>
+          <li class="nav-item d-grid" role="presentation">
+            <button
+              class="nav-link"
+              id="pills-heridas-tab"
+              data-bs-toggle="pill"
+              data-bs-target="#pills-heridas"
+              type="button"
+              role="tab"
+              aria-controls="pills-heridas"
+              aria-selected="false"
+            >
+              Manejo de heridas
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
     <div class="tab-content pl-5 pr-5" id="pills-tabContent">
       <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
         <div class="card shadow-lg p-3 mb-5 bg-body-tertiary rounded">
@@ -44,15 +62,22 @@
               style="font-size: 35px; margin-left: 15px; color: #d44a4a; display: block; margin: auto; padding: 20px"
             />
             <p>Asesoria sobre la prevención de enfermedades. Diagnóstico y tratamiento.</p>
-            <button
-              type="button"
-              class="btn btn-primary d-block m-auto"
-              data-bs-toggle="modal"
-              data-bs-target="#bookingModal"
-              @click="click()"
-            >
-              Reservar
-            </button>
+            <div class="text-center">
+              <button
+                type="button"
+                class="btn btn-primary"
+                data-bs-toggle="modal"
+                data-bs-target="#bookingModal"
+                @click="click()"
+                id="apple"
+                v-show="isBooked"
+              >
+                Reservar
+              </button>
+            </div>
+            <div class="text-center">
+              <a v-show="!isBooked" class="btn btn-primary" href="https://christianici17.youcanbook.me">Agendar</a>
+            </div>
           </div>
         </div>
       </div>
@@ -68,24 +93,61 @@
               En las consultas se solicitarán examenes complementarios, se instaurará tratamientos y en algunos casos
               hay patologias que requieren resolución quirúrgica, procedimientos u otros.
             </p>
-            <button class="btn btn-primary d-block m-auto">Reservar</button>
+            <div class="text-center">
+              <button
+                type="button"
+                class="btn btn-primary"
+                data-bs-toggle="modal"
+                data-bs-target="#bookingModal"
+                @click="click()"
+                id="apple"
+                v-show="isBooked"
+              >
+                Reservar
+              </button>
+            </div>
+            <div class="text-center">
+              <a v-show="!isBooked" class="btn btn-primary" href="https://christianici17.youcanbook.me">Agendar</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="tab-pane fade pl-5 pr-5" id="pills-heridas" role="tabpanel" aria-labelledby="pills-heridas-tab">
+        <div class="card shadow-lg p-3 mb-5 bg-body-tertiary rounded">
+          <div class="details">
+            <!-- <h3 style="display: inline-flex">Cirugías</h3> -->
+            <font-awesome-icon
+              :icon="['fas', 'bone']"
+              style="font-size: 35px; margin-left: 15px; color: #d44a4a; display: block; margin: auto; padding: 20px"
+            />
+            <p>El manejo de heridas</p>
+            <div class="text-center">
+              <button
+                type="button"
+                class="btn btn-primary"
+                data-bs-toggle="modal"
+                data-bs-target="#bookingModal"
+                @click="click()"
+                id="apple"
+                v-show="isBooked"
+              >
+                Reservar
+              </button>
+            </div>
+            <div class="text-center">
+              <a v-show="!isBooked" class="btn btn-primary" href="https://christianici17.youcanbook.me">Agendar</a>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    <!-- <div class="modal-dialog modal-dialog-centered" id="bookingModal" tabindex="-1" aria-hidden="true"> -->
-    <!-- <MercadoPagoComponent /> -->
-    <!-- </div> -->
-    <!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#bookingModal">
-      Launch demo modal
-    </button> -->
 
     <!-- Modal -->
     <div class="modal fade" id="bookingModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Reserva</h5>
+            <h5 class="modal-title" id="exampleModalLabel">Confirma tu reserva</h5>
             <button
               type="button"
               class="btn-close"
@@ -95,7 +157,6 @@
             ></button>
           </div>
           <div class="modal-body">
-            <p>nada para mostrar</p>
             <MercadoPagoComponent />
           </div>
           <div class="modal-footer"></div>
@@ -106,24 +167,41 @@
 </template>
 <script>
 import MercadoPagoComponent from "./MercadoPagoComponent.vue";
+import { useRoute } from "nuxt/app";
 let tempWindows = null;
 export default {
+  data() {
+    return {
+      isBooked: false,
+    };
+  },
   components: {
     MercadoPagoComponent,
   },
+  mounted() {
+    const route = useRoute();
+    const click = route.query.click;
+
+    if (click) {
+      this.isBooked = true;
+      document.getElementById("apple").click();
+    }
+  },
   methods: {
     async click(type) {
-      if (sessionStorage.getItem("purchased")) {
-        Calendly.initPopupWidget({ url: "https://calendly.com/milasvet-2024/30min" });
-        return;
-      }
+      //Calendly.initPopupWidget({ url: "https://calendly.com/milasvet-2024/30min" });
+
+      // if (sessionStorage.getItem("purchased")) {
+      //   Calendly.initPopupWidget({ url: "https://calendly.com/milasvet-2024/30min" });
+      //   return;
+      // }
       const config = useRuntimeConfig();
       const mp = new MercadoPago(config.public.meli, { locale: "es-CL" });
       const bricksBuilder = mp.bricks();
       const renderPaymentBrick = async (bricksBuilder) => {
         const settings = {
           initialization: {
-            amount: 100,
+            amount: 15000,
             // preferenceId: "<PREFERENCE_ID>",
             payer: {
               firstName: "",
@@ -160,7 +238,7 @@ export default {
             onSubmit: async ({ selectedPaymentMethod, formData }) => {
               try {
                 let paymentResponse = null;
-                await fetch("https://us-central1-vetsly.cloudfunctions.net/milasbooking", {
+                fetch("https://us-central1-vetsly.cloudfunctions.net/milasbooking", {
                   method: "POST",
                   headers: {
                     "Content-Type": "application/json",
@@ -170,10 +248,9 @@ export default {
                   .then((response) => response.json())
                   .then((res) => {
                     paymentResponse = res;
-                    console.log("morris", paymentResponse);
-                    if (paymentResponse.status == "approved") {
-                      sessionStorage.setItem("purchased", true);
-                    }
+                    // if (paymentResponse.status == "approved") {
+                    //   sessionStorage.setItem("purchased", true);
+                    // }
                     this.renderStatusScreenBrick(bricksBuilder, paymentResponse);
                   });
                 // renderStatusScreenBrick(bricksBuilder);
@@ -213,8 +290,8 @@ export default {
       );
       setTimeout(() => {
         document.getElementById("statusScreenBrick_container").style.display = "none";
-        Calendly.initPopupWidget({ url: "https://calendly.com/milasvet-2024/30min" });
-      }, 6000);
+        // Calendly.initPopupWidget({ url: "https://calendly.com/milasvet-2024/30min" });
+      }, 8000);
     },
   },
 };
